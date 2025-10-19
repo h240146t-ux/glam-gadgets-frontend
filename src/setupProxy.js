3e6fed46b60a19b1e6ec8c7b8146e415cabbee56
h240146t-ux <h240146t@hit.ac.zw>
2025-10-19 11:20:36 +0200

    Change proxy target to production server

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:5000',
+      target: 'https://glam-gadgets-backend.onrender.com',
       changeOrigin: true,
       secure: false,
       logLevel: 'debug',
